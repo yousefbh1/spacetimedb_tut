f06e465300218b3c85ff5ae3345e5cf0c22f7dc8
Yousef Thulqarnain <yousefbh@umich.edu>
2025-04-03 20:41:43 -0400

    chat client using spacetimedb

--- quickstart-chat/client/obj/Debug/net8.0/client.AssemblyInfo.cs
+++ quickstart-chat/client/obj/Debug/net8.0/client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52058fa34aff4fbb3e0f2a64320b28428b652bbe")]
 [assembly: System.Reflection.AssemblyProductAttribute("client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
